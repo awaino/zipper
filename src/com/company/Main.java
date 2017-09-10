0494bbcd461c5052253fa138dae2a82537a3ce4a
awaino <awaino@yahoo.com>
2017-09-10 16:46:43 -0600

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,30 @@ import java.util.zip.*;
 
 public class Main {
 
+    public static byte[] bytes;
 
     public static void main(String[] args) throws IOException {
-        StringBuilder textToCompress = new StringBuilder();
+
 
         boolean compressed = isCompressed(args[0]);
         String filePath = args[1];
         System.out.println("File path is: " + filePath);
         System.out.println("Compression status is: " + compressed);
-        try {
-            getFileContent(filePath, textToCompress);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        compress(textToCompress);
 
+        if(compressed == true) {
+
+            try {
+                getFileContent(filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Bytes size: " + bytes.length);
+            compress(filePath);
+        }
+        if(compressed == false){
+            System.out.println("Decompressing " + filePath);
+            decompress(filePath);
+        }
     }
     private static boolean isCompressed(String compressing){
         if (compressing.equals("compress"))
@@ -28,29 +37,64 @@ public class Main {
         return false;
     }
 
-    private static String compress(StringBuilder textToCompress){
+    private static String compress(String filePath) throws IOException {
 
+        FileOutputStream out= new FileOutputStream(filePath+".GZ");
+        GZIPOutputStream GZout = new GZIPOutputStream(out);
+        //int a = 0;
+        System.out.println("Char array length: " + bytes.length);
+        //for (a = 0; a < bytes.length;a++){
+            //System.out.println("Char array position: " + a);
+            //System.out.println("Character: " + bytes[a]);
+            GZout.write(bytes, 0, bytes.length);
+       // }
 
-        System.out.println("Text to compress is: " + textToCompress);
-        return "Compress working";
+        GZout.close();
+    return "compress working.";
     }
 
+    private static String decompress(String filepath) throws IOException {
+        FileInputStream in = new FileInputStream(filepath);
+        GZIPInputStream GZin = new GZIPInputStream(in);
+        int a = 0;
+        bytes = new byte[1000];
+        System.out.println("bytes size is: " + bytes.length);
+        System.out.println("Zip file elements: " + GZin.available());
+        for (a = 0; a < GZin.available();a++){
+            System.out.println("Char array position: " + a);
+            //System.out.println("Character: " + bytes[a]);
+            int ch = GZin.read();
+            bytes[a] = (byte) ch;
+            System.out.println("Decompressed char is: " + ((char)bytes[a]));
 
-    private static String decompress(String textToDecompress){
+        }
+
+        GZin.close();
         return "Decompress working.";
     }
 
-    private static void getFileContent (String filePath, StringBuilder textToCompress) throws IOException {
+    private static void getFileContent (String filePath) throws IOException {
         FileInputStream input= new FileInputStream(filePath);
         int f = 0;
-
+        char ch = 0;
         System.out.println("Total characters available: " + input.available());
-            while ((f=input.read())> 0) {
-                //System.out.println("Character read: Byte = " + f + " Character = " + ((char)f));
-                textToCompress.append((char)f);
-                //System.out.println("Text to compress is: " + textToCompress);
+        bytes = new byte[input.available()];
+        System.out.println("Size of array is " + (bytes.length));
+            for (f = 0; f < bytes.length; f++){
+                ch = (char) input.read();
+                //System.out.println("Character read: " + f);
+                //System.out.println("Array position is: " + f);
+                bytes[f] = (byte) ch;
+                //System.out.println("Text to compress is: " + bytes[f]);
+                //if (f < input.available()) {
+                //    System.out.println("Next array position to load is: " + f);
+                //}
             }
-            input.close();
+            System.out.println("\nTotal characters available: " + input.available());
+            System.out.println("Size of array is " + (bytes.length));
+            System.out.println("Array contains: " + Arrays.toString(bytes));
+
+        input.close();
     }
 
 }
